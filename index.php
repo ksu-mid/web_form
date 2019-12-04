c3cc7372186bec4734e14503d427cdecea60afc6
ksu-mid <48205739+ksu-mid@users.noreply.github.com>
2019-12-04 17:56:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?$APPLICATION->IncludeComponent(
+	"bitrix:form.result.new",
+	"feedback_web_form",
+	Array(
+		"CACHE_TIME" => "3600",
+		"CACHE_TYPE" => "A",
+		"CHAIN_ITEM_LINK" => "",
+		"CHAIN_ITEM_TEXT" => "",
+		"EDIT_URL" => "",
+		"IGNORE_CUSTOM_TEMPLATE" => "N",
+		"LIST_URL" => "",
+		"SEF_MODE" => "N",
+		"USE_EXTENDED_ERRORS" => "N",
+		"VARIABLE_ALIASES" => Array("RESULT_ID"=>"RESULT_ID","WEB_FORM_ID"=>"WEB_FORM_ID"),
+		"WEB_FORM_ID" => "1",
+		"AJAX_MODE" => "Y",
+		"AJAX_OPTION_JUMP" => "Y",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+	
+	)
+);?>
